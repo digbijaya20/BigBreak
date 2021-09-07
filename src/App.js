10721166779ea519b2dd45ffa2e0ba735cf5add5
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2021-09-07 17:35:39 +0530

    ui wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+// import demo from './Assets/SvgImages/demo.svg'
+import { Box, Container } from '@material-ui/core';
 import './App.css';
+import NavBar from './Components/AppBar'
+import PerfectPlace from './Components/PerfectPlace';
+import HowItActs from './Components/HowItActs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App" mt={4}>
+      <Container >
+        {/* <img src={demo} className="App-logo" alt="logo" /> */}
+        <NavBar/>
+        <PerfectPlace/>
+      </Container>
+      <Box style={{backgroundColor:"#fff"}} mt={4}>
+      <HowItActs/>
+      </Box>
+
+    </Box>
   );
 }
 
---
+++ src/Components/PerfectPlace.js
@@ -0,0 +1,71 @@
+import { Box, Button, Card, CardContent, Grid, Typography } from '@material-ui/core';
+import perfectPlace from '../Assets/SvgImages/perfectPlace-2.svg';
+import { alpha, makeStyles } from '@material-ui/core/styles';
+// import DeleteIcon from '@material-ui/icons/Delete';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+const useStyles = makeStyles((theme) => ({
+    
+      imageStyle:{
+        width:'90%',
+        
+    },
+    subText:{
+        color:'rgba(0, 0, 0, 0.54)',
+        marginTop:"7px",
+    },
+    cardStyle: {
+        maxWidth: 350,
+        color:'rgba(0, 0, 0, 0.45)',
+        marginTop:"15px",
+        // boxShadow:'none',
+      },
+      btnStyle:{
+        marginTop:"10px",
+          backgroundColor:"#70d1e0",
+      }
+
+
+}))
+const PerfectPlace = () =>{
+    const classes = useStyles();
+    return(
+        <Box mt={4}>
+            <Grid container>
+                <Grid item sm={12} xs={12} md={6} lg={6} >
+                    <Typography className={classes.menuItems} variant="h2" >
+                        Perfect place for your business promotion
+                    </Typography>
+                    <Typography className={classes.subText} variant="h6" color="default" >
+                        Businesses generally promote their brand, products,<br/> and services 
+                        by identifying audiance
+                    </Typography>
+                    <Button className={classes.btnStyle} endIcon={<NavigateNextIcon/>}>
+                        Get Start
+                    </Button>
+                    <Card className={classes.cardStyle}>
+                        
+                        <CardContent>
+                        <Typography variant="h5" component="h5" 
+                        style={{color:"#70d1e0",fontSize:"30px", fontWeight:"bold"}}
+                        >
+                            ''
+                        </Typography>
+
+                        There is no shortage of ideas, what's missing is the will to execute them. ~ 
+                        <span style={{fontWeight:"bold"}}>Seth Godin</span>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sm={12} xs={12} md={6} lg={6} >
+                <Box >
+                    <img src={perfectPlace} alt="perfct Place" className={classes.imageStyle} />
+                </Box>
+                </Grid>
+            </Grid>
+            
+        </Box>
+    )
+}
+
+export default PerfectPlace;
